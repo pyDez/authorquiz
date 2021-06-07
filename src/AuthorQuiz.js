f731ba07e0be8592e8dc48d3aba3ed0602030618
Pierre-Yves Dezaunay <py.dezaunay@gmail.com>
2021-06-07 06:21:25 +0200

    Init git repo

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css'
+
+function AuthorQuiz() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Author Quiz</h1>
+      </header>
+    </div>
+  );
+}
+
+export default AuthorQuiz;
